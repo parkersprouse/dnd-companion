b0081646001afec1735b691752ef2355de9ec3a6
Parker Sprouse <poker158149@gmail.com>
2017-12-18 00:32:12 -0600

    mobile nav

--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarGroup, AnchorButton, Popover, Menu, MenuItem, Position, PopoverInteractionKind } from '@blueprintjs/core';
+import { Navbar, NavbarGroup, AnchorButton, Popover, Menu, MenuItem, Position, PopoverInteractionKind, Button, Collapse } from '@blueprintjs/core';
 
 export default class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.userDropdown =
+    this.state = {
+      isOpen: false
+    }
+
+    this.configureDesktopMenu = this.configureDesktopMenu.bind(this);
+
+    this.isMobile = false;
+    if (window.innerWidth < 992)
+      this.isMobile = true;
+  }
+
+  configureDesktopMenu() {
+    let userDropdown =
       <Menu>
         <MenuItem text="Settings" iconName="cog" href="/profile" shouldDismissPopover={false} />
         <MenuItem text="Logout" iconName="log-out" href="/logout" shouldDismissPopover={false} />
       </Menu>;
 
-    this.rightSide = null;
+    let rightSide = null;
     const auth = false;
     if (auth) {
-      this.rightSide =
+      rightSide =
         <NavbarGroup align="right">
-          <Popover content={this.userDropdown} position={Position.BOTTOM}
+          <Popover content={userDropdown} position={Position.BOTTOM}
                    interactionKind={PopoverInteractionKind.HOVER}
                    hoverOpenDelay={0} hoverCloseDelay={150}>
             <AnchorButton className="pt-minimal" iconName="user" rightIconName="caret-down">User</AnchorButton>
@@ -24,25 +36,47 @@ export default class Header extends Component {
         </NavbarGroup>;
     }
     else {
-      this.rightSide =
+      rightSide =
         <NavbarGroup align="right">
           <AnchorButton href="/login" className="pt-minimal" iconName="log-in">Login</AnchorButton>
           <AnchorButton href="/register" className="pt-minimal" iconName="new-person">Register</AnchorButton>
         </NavbarGroup>;
     }
-  }
 
-  render() {
-    return (
+    this.menu = (
       <div className="heading">
         <Navbar>
           <NavbarGroup>
             <AnchorButton href="/" className="pt-minimal" iconName="home">Home</AnchorButton>
           </NavbarGroup>
-          { this.rightSide }
+          { rightSide }
         </Navbar>
       </div>
     );
+  }
+
+  render() {
+    if (this.isMobile) {
+      this.menu = (
+        <div className="heading">
+          <Navbar>
+            <NavbarGroup align="right">
+              <Button onClick={() => this.setState({ isOpen: !this.state.isOpen })} iconName="menu"></Button>
+            </NavbarGroup>
+          </Navbar>
+          <Collapse isOpen={this.state.isOpen}>
+            <AnchorButton href="/" className="pt-minimal">Home</AnchorButton>
+            <AnchorButton href="/profile" className="pt-minimal">Profile</AnchorButton>
+            <AnchorButton href="/logout" className="pt-minimal">Logout</AnchorButton>
+          </Collapse>
+        </div>
+      );
+    }
+    else {
+      this.configureDesktopMenu();
+    }
+
+    return this.menu;
   }
 
 }
--- client/src/js/components/LoginForm.js
+++ client/src/js/components/LoginForm.js
@@ -4,6 +4,17 @@ import { Button, Intent } from '@blueprintjs/core';
 export default class LoginPage extends Component {
   constructor(props) {
     super(props);
+
+    this.renderError = this.renderError.bind(this);
+    this.submit = this.submit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+
+    this.state = {
+      errorMsg: null,
+      email: '',
+      password: '',
+      isSubmitting: false
+    }
   }
 
   render() {
@@ -13,31 +24,64 @@ export default class LoginPage extends Component {
         <div className="row">
           <div className="col-xs-12 col-md-4 col-md-offset-4">
             <div className="pt-card">
-              <div className="pt-form-group">
-                <label className="pt-label" htmlFor="email-input">
-                  E-mail
-                </label>
-                <div className="pt-form-content">
-                  <input id="email-input" className="pt-input" style={{ width: '100%' }} placeholder="E-mail" type="text" />
+              { this.renderError() }
+              <form onSubmit={this.submit}>
+                <div className="pt-form-group">
+                  <label className="pt-label" htmlFor="email-input">
+                    E-mail
+                  </label>
+                  <div className="pt-form-content">
+                    <input id="email-input" name="email" className="pt-input" style={{ width: '100%' }} placeholder="E-mail" type="text" value={this.state.email} onChange={this.handleInputChange} />
+                  </div>
                 </div>
-              </div>
-              <div className="pt-form-group">
-                <label className="pt-label" htmlFor="password-input">
-                  Password
-                </label>
-                <div className="pt-form-content">
-                  <input id="password-input" className="pt-input" style={{ width: '100%' }} placeholder="Password" type="password" />
+                <div className="pt-form-group">
+                  <label className="pt-label" htmlFor="password-input">
+                    Password
+                  </label>
+                  <div className="pt-form-content">
+                    <input id="password-input" name="password" className="pt-input" style={{ width: '100%' }} placeholder="Password" type="password" value={this.state.password} onChange={this.handleInputChange} />
+                  </div>
                 </div>
-              </div>
-              <div className="pt-form-group" style={{ marginBottom: '0' }}>
-                <div className="pt-form-content" style={{ textAlign: 'center', marginTop: '1rem' }}>
-                  <Button iconName="log-in" intent={Intent.PRIMARY}>Login</Button>
+                <div className="pt-form-group" style={{ marginBottom: '0' }}>
+                  <div className="pt-form-content" style={{ textAlign: 'center', marginTop: '1rem' }}>
+                    <Button iconName="log-in" intent={Intent.PRIMARY} type="submit" loading={this.state.isSubmitting}>Login</Button>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  renderError() {
+    if (this.state.errorMsg) {
+      return (
+        <div className="pt-callout pt-intent-danger" style={{ color: '#AA0000', marginBottom: '2rem' }}>
+          { this.state.errorMsg }
+        </div>
+      );
+    }
+    return null;
+  }
+
+  submit(event) {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({
+      isSubmitting: true,
+      errorMsg: null
+    });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 }
