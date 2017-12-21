f1361416db2c5860d882b77be2dc2bc622f28f6f
Parker <psprouse@uno.edu>
2017-12-21 14:59:21 -0600

    successfully set up sequelize

--- server/db.js
+++ server/db.js
@@ -11,6 +11,28 @@ var options = {
 var pgp = require('pg-promise')(options);
 var db = pgp(config.dbConfig);
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  database: config.dbConfig.database,
+  username: config.dbConfig.user,
+  password: config.dbConfig.password,
+  host: config.dbConfig.host,
+  port: config.dbConfig.port,
+  ssl: config.dbConfig.ssl,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: config.dbConfig.ssl
+  }
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 module.exports = {
   db: db,
   pgp: pgp
