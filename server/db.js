1df5475368e03cdf2c079df9444291e0ceff8a61
Parker <psprouse@uno.edu>
2017-12-21 15:32:13 -0600

    removed test authentication

--- server/db.js
+++ server/db.js
@@ -1,17 +1,18 @@
 // eslint-disable-next-line
 'use strict';
 
-const bluebird = require('bluebird');
+//const bluebird = require('bluebird');
+
+// var options = {
+//   promiseLib: bluebird // Switch our db promises to use bluebird instead of ES6 Promises
+// };
+
+// var pgp = require('pg-promise')(options);
+// var db = pgp(config.dbConfig);
+
 const config = require('./config');
-
-var options = {
-  promiseLib: bluebird // Switch our db promises to use bluebird instead of ES6 Promises
-};
-
-var pgp = require('pg-promise')(options);
-var db = pgp(config.dbConfig);
-
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize({
   database: config.dbConfig.database,
   username: config.dbConfig.user,
@@ -24,14 +25,6 @@ const sequelize = new Sequelize({
     ssl: config.dbConfig.ssl
   }
 });
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 module.exports = {
   db: db,
