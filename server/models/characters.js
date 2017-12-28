004193a742c2e69a8721a5befd718f794bcd35b6
Parker Sprouse <poker158149@gmail.com>
2017-12-28 09:32:42 -0600

    fleshed out character model more

--- server/models/characters.js
+++ server/models/characters.js
@@ -2,17 +2,44 @@ const { db, Sequelize } = require('../config/db');
 const Users = require('./Users');
 
 const attributes = {
-  id:         { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, unique: true, primaryKey: true },
-  name:       { type: Sequelize.TEXT, allowNull: false },
-  level:      { type: Sequelize.INTEGER, allowNull: false, defaultValue: 1 },
-  experience: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
-  userid:     { type: Sequelize.INTEGER, allowNull: false,
-                references: {
-                  model: Users,
-                  key: 'id',
-                  deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-                }
-              }
+  id:                { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, unique: true, primaryKey: true },
+  name:              { type: Sequelize.TEXT, allowNull: false },
+  level:             { type: Sequelize.INTEGER, allowNull: false, defaultValue: 1 },
+  experience:        { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
+  alignment:         { type: Sequelize.TEXT, allowNull: false },
+  race:              { type: Sequelize.INTEGER, allowNull: false }, // race ID
+  background:        { type: Sequelize.TEXT, allowNull: false },
+  class:             { type: Sequelize.INTEGER, allowNull: false }, // class ID
+  proficiency_bonus: {},
+  inspiration:       {},
+  armor_class:       {},
+  speed:             {},
+  max_hp:            {},
+  current_hp:        {},
+  temp_hp:           {},
+  hit_dice:          {},
+  equipment:         { type: Sequelize.ARRAY(Sequelize.INTEGER), allowNull: false }, // array of equipment IDs
+  notes:             {},
+  traits:            {},
+  personality:       {},
+  ideals:            {},
+  bonds:             {},
+  flaws:             {},
+  platinum:          {},
+  gold:              {},
+  electrum:          {},
+  silver:            {},
+  copper:            {},
+  proficiencies:     { type: Sequelize.ARRAY(Sequelize.INTEGER), allowNull: false }, // array of proficiency IDs
+
+  userid:            {
+                       type: Sequelize.INTEGER, allowNull: false,
+                       references: {
+                         model: Users,
+                         key: 'id',
+                         deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+                       }
+                     }
 };
 
 const tableSettings = {
