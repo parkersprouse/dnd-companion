6e9179a9b8e9f16a4caf8847fd2e691be4c520f9
Parker Sprouse <poker158149@gmail.com>
2018-01-29 01:05:14 -0600

    modal is functional, just not complete

--- client/src/js/pages/SpellSearcher.js
+++ client/src/js/pages/SpellSearcher.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-//import { Input } from '@blueprintjs/core';
+import { Grid, List } from 'semantic-ui-react';
+import { Dialog, Button } from '@blueprintjs/core';
 import OuterContainer from '../components/OuterContainer';
 import InnerContainer from '../components/InnerContainer';
 import Header from '../components/Header';
@@ -15,7 +15,8 @@ export default class SpellSearcher extends Component {
       name_filter: '',
       level_filter: '',
       class_filter: '',
-      school_filter: ''
+      school_filter: '',
+      selected_spell: null
     };
   }
 
@@ -132,8 +133,177 @@ export default class SpellSearcher extends Component {
 
           </Grid>
 
+          { this.renderSpellModal() }
+
         </InnerContainer>
       </OuterContainer>
+    );
+  }
+
+  renderSpellModal = () => {
+    const spell = this.state.selected_spell;
+    if (!spell) return null;
+
+    const spell_classes = _.map(spell.classes, (sc) => { return sc.name });
+
+    return (
+      <Dialog isOpen={!!spell} title={spell.name}
+              onClose={() => this.setState({ selected_spell: null })}>
+        <div className='pt-dialog-body'>
+          <List>
+
+            {
+              spell.level ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Level</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.level }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.desc ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Description</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.desc[0] }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.higher_level ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Higher Level</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.higher_level[0] }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.range ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Range</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.range }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.duration ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Duration</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.duration }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.casting_time ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Casting Time</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.casting_time }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.components ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Components</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ _.join(spell.components, ', ') } { spell.components.indexOf('M') > -1 ? <span>({spell.material})</span> : null}</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.school && spell.school.name ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>School</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ spell.school.name }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+            {
+              spell.classes ?
+              <List.Item>
+                <List.Content>
+                  <List.Header>Classes</List.Header>
+                  <List.List>
+                    <List.Item>
+                      <List.Content>
+                        <List.Description>{ _.join(spell_classes, ', ') }</List.Description>
+                      </List.Content>
+                    </List.Item>
+                  </List.List>
+                </List.Content>
+              </List.Item> : null
+            }
+
+          </List>
+        </div>
+        <div className='pt-dialog-footer'>
+          <div className='pt-dialog-footer-actions'>
+            <Button onClick={() => this.setState({ selected_spell: null })} text='Close' />
+          </div>
+        </div>
+      </Dialog>
     );
   }
 
@@ -142,7 +312,7 @@ export default class SpellSearcher extends Component {
     _.each(this.state.spells, (spell) => {
       const spell_classes = _.map(spell.classes, (sc) => { return sc.name });
       all_spells.push(
-        <tr key={spell.index}>
+        <tr key={spell.index} onClick={() => { this.setState({ selected_spell: spell }) }}>
           <td>{ spell.level === 0 ? 'Cantrip' : spell.level }</td>
           <td>{ spell.name }</td>
           <td>{ spell.range }</td>
@@ -180,7 +350,7 @@ export default class SpellSearcher extends Component {
     _.each(filtered_spells, (spell) => {
       const spell_classes = _.map(spell.classes, (sc) => { return sc.name });
       all_spells.push(
-        <tr key={spell.index}>
+        <tr key={spell.index} onClick={() => { this.setState({ selected_spell: spell }) }}>
           <td>{ spell.level === 0 ? 'Cantrip' : spell.level }</td>
           <td>{ spell.name }</td>
           <td>{ spell.range }</td>
