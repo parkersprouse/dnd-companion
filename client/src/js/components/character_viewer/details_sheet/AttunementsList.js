a4e72a5a561a69ad663c317d9b8da4f1eda7c507
Parker <psprouse@uno.edu>
2018-03-27 20:38:11 -0500

    Added AttunementsList and AttunementsTreeDisplay; attunements are fully functional

--- client/src/js/components/character_viewer/details_sheet/AttunementsList.js
+++ client/src/js/components/character_viewer/details_sheet/AttunementsList.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import { Position, Toaster, Intent } from '@blueprintjs/core';
+import { Button, Intent, MenuItem, Position, Toaster } from '@blueprintjs/core';
+import { Select } from '@blueprintjs/labs';
 import _ from 'lodash';
 import api from '../../../lib/api';
-//import WeaponsTreeDisplay from './WeaponsTreeDisplay';
-//import WeaponSelector from '../../character_creation/selectors/WeaponSelector';
+import AttunementsTreeDisplay from './AttunementsTreeDisplay';
 
 export default class AttunementsList extends Component {
   render() {
+    let options = _.map(this.props.character.equipment, 'name');
+    options = _.concat(options, _.map(this.props.character.weapons, 'name'));
+    options = _.concat(options, _.map(this.props.character.armor, 'name'));
+    options = _.sortBy(options, (i) => i);
+
     return (
       <div className='pt-form-group' style={{ marginBottom: '0' }}>
         <div className='pt-form-content searcher'>
-          Attunements
-          {/* <WeaponsTreeDisplay character={this.props.character}
-                              content={this.props.character.attunements}
-                              remove={this.removeAttunement}
-                              setRootState={this.props.setRootState} />
-          <WeaponSelector addAttunement={this.addAttunement} centered /> */}
+          <AttunementsTreeDisplay character={this.props.character}
+                                  content={this.props.character.attunements.items}
+                                  remove={this.removeAttunement}
+                                  setRootState={this.props.setRootState} />
+          <div style={{ marginTop: '0.5rem' }}>
+            <Select
+              items={options}
+              itemPredicate={ (query, selected) => selected.toLowerCase().indexOf(query.toLowerCase()) >= 0 }
+              itemRenderer={ ({ handleClick, isActive, item }) => {
+                const style = isActive ? 'pt-active pt-intent-primary' : '';
+                return <MenuItem className={style} label={null} key={item} onClick={handleClick} text={item} />
+              } }
+              onItemSelect={ (selected) => this.addAttunement(selected) }
+              popoverProps={{ minimal: true, placement: 'top' }}
+              noResults={<MenuItem disabled text='No results' />}
+              resetOnSelect={true}
+            >
+              <Button className='pt-fill text-left dropdown-btn' rightIconName='caret-down' text={'Choose Item'} />
+            </Select>
+            <div className='pt-form-helper-text text-center'>Attunements</div>
+          </div>
         </div>
       </div>
     );
   }
 
   addAttunement = (attunement, custom) => {
-    const attunements = this.props.character.attunements;
+    const attunements = this.props.character.attunements || { amount: 0, items: [] };
     if (attunements.items.indexOf(attunement) > -1 || attunements.amount >= 3) return;
     attunements.items.push(attunement);
     attunements.amount++;
---
+++ client/src/js/components/character_viewer/details_sheet/AttunementsTreeDisplay.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+export default class AttunementsTreeDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: props.content && props.content.length > 0 ?
+      _.sortBy(props.content, (i) => i) : []
+    }
+  }
+
+  componentWillReceiveProps(next_props) {
+    this.setState({
+      content: next_props.content && next_props.content.length > 0 ?
+      _.sortBy(next_props.content, (i) => i) : []
+    });
+  }
+
+  render() {
+    const content = this.renderContent();
+    return (
+      <div className='pt-tree pt-elevation-0'>
+        <ul className='pt-tree-node-list pt-tree-root'>
+          {
+            content.length > 0 ? content :
+            <li className='pt-tree-node'>
+              <div className='pt-tree-node-content'>
+                <span className='pt-tree-node-label' style={{ paddingLeft: '10px' }}><i>None</i></span>
+              </div>
+            </li>
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  renderContent = () => {
+    return _.map(this.state.content, (ele, index) => {
+      return (
+        <li key={index} className='pt-tree-node'>
+          <div className='pt-tree-node-content'>
+            <span className='pt-tree-node-label' style={{ marginLeft: '0.75rem' }}>
+              {ele}
+            </span>
+            <span className='pt-tree-node-secondary-label'>
+              <a onClick={() => this.props.remove(ele)} className='remove-item-btn'>
+                <span className='pt-icon-cross'></span>
+              </a>
+            </span>
+          </div>
+        </li>
+      );
+    });
+  }
+}
