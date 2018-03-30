a45026d4880d439f7378a1c5a540a80fa145655b
Parker Sprouse <parker@parkersprouse.me>
2018-03-29 22:25:58 -0500

    clean up some unneeded formlabel code

--- client/src/js/components/FormLabel.js
+++ client/src/js/components/FormLabel.js
@@ -1,27 +1,9 @@
 import React, { Component } from 'react';
 
 export default class FormLabel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      required: this.props.required !== undefined
-    }
-  }
-
   render() {
-    if (this.state.required) {
-      return (
-        <span>
-          { this.props.children } <span className='required-label no-icon'>*</span>
-        </span>
-      );
-    }
-    else {
-      return (
-        <span>
-          { this.props.children }
-        </span>
-      );
-    }
+    if (this.props.required !== undefined)
+      return <span>{ this.props.children } <span className='required-label no-icon'>*</span></span>;
+    return <span>{ this.props.children }</span>;
   }
 }
